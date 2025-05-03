080eb1fc02ffc9422e33efb5cda8044ed7a961d6
Dlurak <84224239+Dlurak@users.noreply.github.com>
2025-05-03 11:06:42 +0200

    Add a flake

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    utils,
+  }:
+    utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {inherit system;};
+      in {
+        defaultPackage = let
+          manifest = pkgs.lib.importTOML ./Cargo.toml;
+        in
+          pkgs.rustPlatform.buildRustPackage {
+            pname = manifest.package.name;
+            version = manifest.package.version;
+
+            src = pkgs.lib.cleanSource ./.;
+            cargoLock.lockFile = ./Cargo.lock;
+          };
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            cargo
+            rustc
+            rustfmt
+            rustPackages.clippy
+            bacon
+			rust-analyzer
+          ];
+          RUST_SRC_PATH = pkgs.rustPlatform.rustLibSrc;
+        };
+      }
+    );
+}
