d32f50cdbcf4fa2388fd5da5c9b332ce5b773b02
oiwn <alex@imscraping.ninja>
2025-10-03 19:26:43 +0700

    update rust toolchain in flake.nix version

--- flake.nix
+++ flake.nix
@@ -27,7 +27,7 @@
               pname = manifest.package.name;
               version = manifest.package.version;
 
-              rust-toolchain = pkgs.rust-bin.stable."1.76.0".default;
+              rust-toolchain = pkgs.rust-bin.stable."1.89.0".default;
 
               src = pkgs.lib.cleanSource ./.;
               cargoLock.lockFile = ./Cargo.lock;
